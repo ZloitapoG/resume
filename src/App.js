c269a8dd794744cf2c64d303f4247737e82d5bd9
Kristina <kamagorovakristina@gmail.com>
2025-09-09 09:16:45 +0700

    Styles connect...

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import {personalData, contacts} from './content/content.js';
+import {educationUniversity, courses} from './content/education.js';
+import experience from './content/experience.js';
+import './styles.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Камагорова Кристина Александровна
     </div>
   );
 }
--- src/content/experience.js
+++ src/content/experience.js
@@ -1,20 +1,11 @@
 const date = new Date(2013, 3)
-function employment(startDate){
-    const endMMYY = new Date();
-    const startMMYY = new Date(startDate);
-    const [startMonth, startYear] = startMMYY.split('.').map(Number);
-    const [endMonth, endYear] = endMMYY.split('.').map(Number);
-    const fullStartYear = 2000 + startYear;
-    const fullEndYear = 2000 + endYear;
-    const totalMonths = (fullEndYear - fullStartYear) * 12 + (endMonth - startMonth);
-    return totalMonths;
-};
+
 const experience = {
     "job title": "Электромонтер станционного оборудования",
     organization: 'АО "Связьтранснефть"',
     locality: "Кузбасс, г. Мариинск",
     start: date.toLocaleString(),
-    stage: employment(start),
+    end: true,
     functional: [],
     skills: [],
     softskills:[],
