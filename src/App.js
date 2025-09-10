7fd8ce5f67fd44f035cb8af2609bb6685189f972
Kristina <kamagorovakristina@gmail.com>
2025-09-10 09:20:29 +0700

    Add title + favicon

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import {personalData, contacts} from './content/content.js';
-import {educationUniversity, courses} from './content/education.js';
+import { personalData, contacts } from './content/content.js';
+import { educationUniversity, courses } from './content/education.js';
 import experience from './content/experience.js';
 import './styles.css';
 
 function App() {
   return (
-    <div className="App">
-      Камагорова Кристина Александровна
+    <div className='a4-container'>
+      <div className='a4-page'>
+        <div className="App">
+          Камагорова Кристина Александровна
+        </div>
+      </div>
     </div>
   );
 }
